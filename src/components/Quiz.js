d9bdadb199c4247c821a7e933c134989e0c2de4f
Constantine <faustlarsen@gmail.com>
2021-02-26 17:43:40 -0700

    add question title

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 
+
 function Quiz() {
+
   const questions = [
     {
+      title: 'Percentages',
       questionText: 'If you owed someone $500 plus interest, which would be higher to pay back?',
       answerOptions: [
 				{ answerText: '$510', isCorrect: false },
@@ -10,6 +13,7 @@ function Quiz() {
 			],
     },
     {
+      title: 'Inflation',
       questionText: 'If both your income and the prices of groceries, gas, and other products you buy were to double over the next 20 years, how much will you be able to purchase?',
       answerOptions: [
 				{ answerText: 'More than you can buy now', isCorrect: false },
@@ -18,6 +22,7 @@ function Quiz() {
 			],
     },
     {
+      title: 'Compound Interest',
       questionText: 'You have $1,000 in a savings account earning 1% annually. What would your balance be after 10 years if you never withdrew any money from your account?',
       answerOptions: [
 				{ answerText: 'More than $1,100', isCorrect: true },
@@ -26,6 +31,7 @@ function Quiz() {
 			],
     },
     {
+      title: 'Impact Of Losses',
       questionText: 'Suppose you lose 50% of a $10,000 investment. How much return would you need to get back to $10,000?',
       answerOptions: [
 				{ answerText: 'A gain of 50%', isCorrect: false },
@@ -34,6 +40,7 @@ function Quiz() {
 			],
     },
     {
+      title: 'Risk Diversification',
       questionText: 'Generally speaking, is it more secure to put your money into:',
       answerOptions: [
 				{ answerText: 'One place', isCorrect: false },
@@ -67,6 +74,7 @@ function Quiz() {
 				</div>) : (
 				<React.Fragment>
 						<span>Question {currentQuestion + 1}</span>/{questions.length}
+            <div className='question-title'>{questions[currentQuestion].title}</div>
 						<div className='question-text'>{questions[currentQuestion].questionText}</div>
 						{questions[currentQuestion].answerOptions.map((answerOption) => (
 							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
