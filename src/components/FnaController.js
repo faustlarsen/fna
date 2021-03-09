23e498d2015108134eb3750c602c1a9faf20273f
Constantine <faustlarsen@gmail.com>
2021-03-08 17:49:15 -0700

    add newClientForm

---
+++ src/components/FnaController.js
@@ -0,0 +1,27 @@
+// import React from 'react';
+// import HouseholdForm from './HouseholdForm';
+// import ClientList from './ClientList';
+// import { connect } from 'react-redux';
+// import PropTypes from "prop-types";
+// import * as a from './../actions';
+// import { withFirestore, isLoaded } from "react-redux-firebase";
+
+
+// class ClientList extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       selectedTicket: null,
+//       editing: false
+//     };
+//   }
+
+// handleAddingNewClientToList = () => {
+//   const { dispatch } = this.props;
+//   const action = a.toggleForm();
+//   dispatch(action);
+// }
+
+
+// export default ClientList;
---
+++ src/components/NewClientForm.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import HouseholdForm from "./HouseholdForm";
+import { useFirestore } from 'react-redux-firebase'
+
+function NewClientForm(props){
+  const firestore = useFirestore();
+  function addClientToFirestore(event) {
+    event.preventDefault();
+    props.onNewClientCreation();
+
+    return firestore.collection('clients').add(
+      {
+        firstName: event.target.firstName.value,
+        lastName: event.target.lastName.value, 
+        phone: event.target.phone.value, 
+        dob: event.target.dob.value,
+      }
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <HouseholdForm 
+        formSubmissionHandler={addClientToFirestore}
+        buttonText="Create!" />
+    </React.Fragment>
+  );
+}
+
+NewClientForm.propTypes = {
+  onNewClientCreation: PropTypes.func
+};
+
+export default NewClientForm;
